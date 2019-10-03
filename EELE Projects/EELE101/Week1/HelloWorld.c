0efc26b91f9404ac709a343769ed0424acd3e8a2
Joshua Harthan <josh_lloyd17@outlook.com>
2019-10-03 00:11:42 -0600

    Add files via upload

---
+++ EELE Projects/EELE101/Week1/HelloWorld.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() 
+{
+ 	//-----------------------------------------------------
+	// Pring the Hello World Message
+	//-----------------------------------------------------
+    printf("Hello World!\n");
+	printf("My name is Joshua Harthan\n");
+ 
+	//-----------------------------------------------------
+	// Keep the console window open until you press a key
+	//-----------------------------------------------------
+	printf("\n\n(Press any key to exit and close the console window)\n");
+    _getch();
+
+    return 0;
+}
