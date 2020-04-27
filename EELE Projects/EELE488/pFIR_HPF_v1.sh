0de7a08b3fc0956aee4850953c4af30675bb739f
Joshua Harthan <joshua.harthan@gmail.com>
2020-04-26 19:10:08 -0600

    Add files via upload

--- EELE Projects/EELE488/pFIR_HPF_v1.sh
+++ EELE Projects/EELE488/pFIR_HPF_v1.sh
@@ -2,16 +2,18 @@
 input="/root/pFIR/hpf_coefficients.txt"
 coeff="/sys/class/fe_pFIR_248/fe_pFIR_248/wr_data"
 address="/sys/class/fe_pFIR_248/fe_pFIR_248/rw_addr"
+enable="/sys/class/fe_pFIR_248/fe_pFIR_248/wr_en"
 
 size=$(stat -c%s "$input")
 
-echo "The size of $input is $size bytes."
+echo "Programming the FIR filter..."
 j=0;
 
 mapfile -t a < "$input"
-for i in "${!a[@]}"; do
-	printf "{a[%s]} = %s\n" "$j" "$((16#${a[i]}))";
+for i in $(seq ${#a[*]}); do
 	echo "$j" | tee "$address" > /dev/null ;
-	echo "$((16#${a[i]}))" | tee "$coeff" > /dev/null ;
+	echo "$((16#${a[$((i - 1))]}))" | tee "$coeff" > /dev/null ;
 	j=$((j+4));
 done
+
+echo "FIR filter successfully programmed!"
--- EELE Projects/EELE488/pFIR_LPF_v1.sh
+++ EELE Projects/EELE488/pFIR_LPF_v1.sh
@@ -2,16 +2,20 @@
 input="/root/pFIR/lpf_coefficients.txt"
 coeff="/sys/class/fe_pFIR_248/fe_pFIR_248/wr_data"
 address="/sys/class/fe_pFIR_248/fe_pFIR_248/rw_addr"
+enable="/sys/class/fe_pFIR_248/fe_pFIR_248/wr_en"
 
 size=$(stat -c%s "$input")
 
-echo "The size of $input is $size bytes."
+echo "Programming the FIR filter..."
 j=0;
+echo 1 > "$enable";
 
 mapfile -t a < "$input"
 for i in "${!a[@]}"; do
-	printf "{a[%s]} = %s\n" "$j" "$((16#${a[i]}))";
 	echo "$j" | tee "$address" > /dev/null ;
 	echo "$((16#${a[i]}))" | tee "$coeff" > /dev/null ;
 	j=$((j+4));
 done
+
+echo 0 > "$enable";
+echo "FIR filter successfully programmed!"
